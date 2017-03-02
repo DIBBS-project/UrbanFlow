f962e3d676d3493778988fabb41271e844b92a3b
Pierre Riteau <priteau@uchicago.edu>
2017-03-02 16:55:44 +0000

    Allow script to run as any user

--- scripts/filter_run_pipeline.sh
+++ scripts/filter_run_pipeline.sh
@@ -2,8 +2,6 @@
 IN="${PREFIX}_tweets"
 OUT="${PREFIX}_${CITYNAME}_tweets"
 
-hadoop dfs -rmr $OUT
-hadoop jar $ROOT_DIR/jars/filter.jar $IN $OUT $minX $maxX $minY $maxY
+hadoop dfs -rmr "/user/${USER}/${OUT}"
+hadoop jar $ROOT_DIR/jars/filter.jar "/user/${USER}/${IN}" "/user/${USER}/${OUT}" ${minX} ${maxX} ${minY} ${maxY}
 echo "Done Filtering Tweets!"
-
-
