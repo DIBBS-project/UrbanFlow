3ebe31127c7872b3b8315307d3f9f401ed46f5ba
Pierre Riteau <priteau@uchicago.edu>
2017-03-16 13:26:07 +0000

    Use absolute paths

--- scripts/chicago_run_pipeline.sh
+++ scripts/chicago_run_pipeline.sh
@@ -11,8 +11,8 @@ CITYNAME="chicago"
 
 source ${SCRIPT_DIR}/filter_run_pipeline.sh
 
-SHAPEFILEADDRESS1="$ROOT_DIR/data/chicago/chicago_landuse_wgs84.shp"
+SHAPEFILEADDRESS1="${ROOT_DIR}/data/chicago/chicago_landuse_wgs84.shp"
 attributes="Reduced"
-source point_in_polygon1.sh
+source ${SCRIPT_DIR}/point_in_polygon1.sh
 
 #Other parts of the scripts require a mongodb instance and will be added later
